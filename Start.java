1d9d36aea7a57e9f4a5541999c3c7f1484f19950
Tico Raap <tico@l-dorp.nl>
2015-11-29 13:40:01 +0100

    Update Start.java

--- Start.java
+++ Start.java
@@ -1,20 +1,22 @@
 package ikopr_valapplicatie;
 
+import javafx.application.Application;
+
 public class Start {
 
 	public static void main(String[] args) throws InterruptedException {
-		ModelValFormule formule = new ModelValFormule(800.0, 0.1, 9.81);
-		formule.setSchermHoogte(600);
-		
-		while (!formule.animatieVoltooid()){
-			formule.nextStepFormule();
-			System.out.println("de gevallen afstand in pixels is: " + formule.getNextSchermPositie());
-			System.out.println("Valsnelheid: " + formule.getValSnelheid());
-			System.out.println("Valafstand: " + formule.getGevallenAfstand());
-			Thread.sleep(100);
 
-		}
+
+		// Aanmaken View
+		ViewValFormule view = new ViewValFormule();
 		
+		// Aanmaken Controller
+		
+
+		
+		
+		Application.launch(view.getClass());
+		System.out.println("jeh");
 	}
 
 }
